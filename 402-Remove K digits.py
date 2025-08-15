1d76f54771adb336340b193d877e751bf081f589
Amina Liaquat <aminaliaquat41@gmail.com>
2025-08-15 10:03:31 +0500

    Create 402-Remove K digits.py

---
+++ 402-Remove K digits.py
@@ -0,0 +1,27 @@
+class Solution:
+    def removeKdigits(self, num: str, k: int) -> str:
+        # If k >= length of number, removing all digits means result is "0"
+        if k >= len(num):
+            return "0"
+        
+        stack = []  # Monotonic increasing stack
+
+        for c in num:
+            # Remove previous larger digits to make number smaller
+            while k > 0 and stack and stack[-1] > c:
+                stack.pop()
+                k -= 1
+            stack.append(c)
+
+        # If we still have k > 0, remove from the end
+        while k > 0:
+            stack.pop()
+            k -= 1
+
+        # Join stack into a string
+        result = ''.join(stack)
+        # Remove leading zeros
+        result = result.lstrip('0')
+
+        # If result is empty, return "0"
+        return result if result else '0'
