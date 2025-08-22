ce3ee058d13797389c79b681f05cf0747671a88e
Amina Liaquat <aminaliaquat41@gmail.com>
2025-08-22 20:56:56 +0500

    Create Removing Stars From a String.py

---
+++ Medium /Removing Stars From a String.py
@@ -0,0 +1,16 @@
+class Solution:
+    def removeStars(self, s: str) -> str:
+        stack = []
+        for i in range(len(s)):
+            # print(s[i])
+            if s[i] == '*':
+                stack.pop() # removing last element / character
+            else:
+                stack.append(s[i])
+        # return stack
+        # return "".join(stack) # converts the elements in list into string
+        a = ""
+        for i in range(len(stack)):
+            a = stack.pop() + a # eocel - lecoe
+            a[::-1] # to reverse the string
+        return a 
