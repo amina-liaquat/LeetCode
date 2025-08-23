6229f310ca8e81b950c4435676370357542ee4ad
Amina Liaquat <aminaliaquat41@gmail.com>
2025-08-23 09:36:35 +0500

    Create Kth Smallest Element in a BST.py

---
+++ Medium /Kth Smallest Element in a BST.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        heap = []
+
+        # heapq.heapify(heap)
+
+        def findKth(root):
+            if not root:
+                return
+            
+            heapq.heappush(heap, -1 * root.val)
+            if len(heap) > k:
+                heapq.heappop(heap)
+
+            findKth(root.left)
+            findKth(root.right)
+        
+        findKth(root)
+        return -1 * heapq.heappop(heap)
+        
