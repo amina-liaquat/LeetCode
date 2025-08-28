79fb16bf973351f25859a24490a95eadc0eaac55
Amina Liaquat <aminaliaquat41@gmail.com>
2025-08-28 09:32:46 +0500

    Create Find and Replace in string.py

---
+++ Medium /Find and Replace in string.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findReplaceString(self, s: str, indices: List[int], sources: List[str], targets: List[str]) -> str:
+        output = str(s[:])
+        shift = 0
+        
+        for index, source, target in sorted(zip(indices, sources, targets)):
+            if s[index:index+len(source)] == source:
+                index += shift
+
+                output = output[:index] + target + output[index+len(source):]
+                shift += len(target) - len(source)
+        
+        return output
+        
