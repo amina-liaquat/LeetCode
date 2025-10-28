934988878d98d6f899b1dda2032593b5e4fb8322
Amina Liaquat <aminaliaquat41@gmail.com>
2025-10-28 10:15:31 +0500

    Create Time needed to buy tickets.py

---
+++ Easy /Time needed to buy tickets.py
@@ -0,0 +1,17 @@
+class Solution:
+    def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
+        q=deque([(i,t) for i,t in enumerate(tickets)])
+        time=0
+        while q:
+            i,t=q.popleft()
+            t-=1
+            time+=1
+            if t>0:
+                q.append((i,t))
+            if i==k and t==0:
+                return time
+
+
+       
+        # T: O(n*m)
+        # S: O(n)
