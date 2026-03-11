81b33977f81cfac7096302214712b8dd56c6d0bf
Amina Liaquat <aminaliaquat41@gmail.com>
2026-03-11 11:27:32 +0500

    Create Symmetric tree.py

---
+++ Easy /Symmetric tree.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        def isMirror(r1,r2):
+            if not r1 and not r2:
+                return True
+            if not r1 or not r2:
+                return False
+            return (r1.val == r2.val) and isMirror(r1.left, r2.right) and isMirror(r1.right, r2.left)
+        if root:
+            return isMirror(root.left,root.right)
+        return True
