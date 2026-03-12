41c4dc5d0765be3c7d295ac9f69bd66a7206514e
Amina Liaquat <aminaliaquat41@gmail.com>
2026-03-12 14:57:19 +0500

    Create Find if Path Exists in Graph.py

---
+++ Easy /Find if Path Exists in Graph.py
@@ -0,0 +1,18 @@
+class Solution:
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        D = defaultdict(list)
+        for u, v in edges:
+            D[u].append(v)
+            D[v].append(u)
+        def dfs(node):
+            if node == destination:
+                return True
+            for nei_node in D[node]:
+                if nei_node not in seen:
+                    seen.add(nei_node)
+                    if dfs(nei_node):
+                        return True
+            return False
+        seen = set()
+        seen.add(source)
+        return dfs(source)
