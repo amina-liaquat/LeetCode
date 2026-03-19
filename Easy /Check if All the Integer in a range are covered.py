d74c5c7fc9cb7b6cf9af26d8cbe05cacf264e73b
Amina Liaquat <aminaliaquat41@gmail.com>
2026-03-19 05:38:17 +0500

    Create Check if All the Integer in a range are covered.py

---
+++ Easy /Check if All the Integer in a range are covered.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isCovered(self, ranges, left, right):
+        covered = [0] * 52
+
+        for start, end in ranges:
+            for i in range(start, end + 1):
+                covered[i] = 1
+
+        for i in range(left, right + 1):
+            if covered[i] == 0:
+                return False
+
+        return True
